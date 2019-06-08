3a3615ba16b88ae4dde65fec77ff40fd9cbd2b4e
sierrakasz <sierrakasz@gmail.com>
2019-06-08 15:55:29 -0400

    Add files via upload

---
+++ pipeline_comparison/fig2.R
@@ -0,0 +1,166 @@
+### Making Figure 2
+
+#packages
+library(HTSSIP)
+library(gridExtra)
+library(ggforce)
+library(ggplot2)
+library(ggpubr)
+library(ggthemes)
+library(PMCMR)
+library(phyloseq)
+library(plyr)
+library(tidyverse)
+library(vegan)
+
+
+# import data -------------------------------------------------------------
+
+
+###tax group is a file that combines the OTU file and taxonomy file. 
+##combining the files made it easier for collapsing differences among the pipeline outputs
+#tax.group is available on github
+
+tax_group <- read.csv("tax_group.csv")
+tax_group <- tax_group %>% group_by(Kingdom, Phylum, Class, Order, Family, Genus) %>% summarise_all(funs(sum))
+tax_group <- tax_group %>%  ungroup()
+
+#this function specifically collapses the OTU table and taxonomy file 
+#when the taxonomy is the same name among pipelines
+regroup_physeq_object <-function(table) {
+  tax <- table %>% select(Kingdom, Phylum, Class, Order, Family, Genus)
+  tax <- as.matrix(tax)
+  otu <- table %>% select(contains("WCME"))
+  OTU=otu_table(otu, taxa_are_rows=TRUE)
+  TAX=tax_table(tax)
+  taxa_names(TAX)=row.names(OTU)
+  physeq_all=phyloseq(OTU,TAX)
+  return(physeq_all)
+}
+
+physeq_all <- regroup_physeq_object(tax_group)
+
+#import metadata and combine into one phyloseq object
+metadata=(read.csv("Metadata/HPMMMeta_r_merge.csv",header=TRUE))
+sampdat=sample_data(metadata)
+sample_names(sampdat)=metadata$SampleID
+physeq=merge_phyloseq(physeq_all, sampdat)
+physeq
+
+#subsetting among both body sites compared
+physeq_rec <- subset_samples(physeq, Sample_Area == "Rectum")
+physeq_mou <- subset_samples(physeq, Sample_Area == "Mouth")
+
+
+# relative_abundance ------------------------------------------------------
+
+
+
+#Tax glom is a function that collapses the taxonomic levels into phylum
+#also are removing very rare taxa (less than 1%)
+GPrPhylum=tax_glom(physeq_rec, "Phylum")
+PhylumLevel_rec = transform_sample_counts(GPrPhylum, function(x) x / sum(x))
+PhylumLevel_rec = filter_taxa(PhylumLevel_rec, function(x) mean(x) > 0.0001, TRUE) 
+
+GPrPhylum=tax_glom(physeq_mou, "Phylum")
+PhylumLevel_mou = transform_sample_counts(GPrPhylum, function(x) x / sum(x))
+PhylumLevel_mou = filter_taxa(PhylumLevel_mou, function(x) mean(x) > 0.0001, TRUE) 
+
+#Relative abundance
+#split by sample area
+#figures include top five phyla, indciated by mean determined in Trtdata
+df <- psmelt(PhylumLevel_mou) 
+df$Abundance=df$Abundance*100
+Trtdata <- ddply(df, c("Phylum", "Pipeline"), summarise,
+                 N    = length(Abundance),
+                 mean = mean(Abundance),
+                 sd   = sd(Abundance),
+                 se   = sd / sqrt(N)
+)
+
+Topfive_trtdata <- filter(Trtdata, Phylum %in% c("Firmicutes", "Bacteroidetes",
+                                                 "Actinobacteria", "Proteobacteria",
+                                                 "Fusobacteria")) 
+#figure 2, panel A
+a <- ggplot(Topfive_trtdata, aes(x=Pipeline, y=mean, fill=Phylum)) + 
+  geom_bar(stat = 'identity', position = position_stack()) +
+  xlab("Mouth") + ylab("Relative Abundance (> 1%)") +
+  scale_x_discrete(labels=c('MG-RAST' = 'MG', 'mothur' = 'M', "QIIME2" = 'Q')) +
+  scale_fill_manual(values=  c("#FF0000", "#006699", "#FF6600", "#FFCC00", "#00A08A")) +
+  theme(legend.position="left")
+a
+
+dfr <- psmelt(PhylumLevel_rec) 
+dfr$Abundance=dfr$Abundance*100
+Trtdatar <- ddply(dfr, c("Phylum", "Pipeline"), summarise,
+                 N    = length(Abundance),
+                 mean = mean(Abundance),
+                 sd   = sd(Abundance),
+                 se   = sd / sqrt(N)
+)
+
+Topfive_trtdatar <- filter(Trtdatar, Phylum %in% c("Firmicutes", "Bacteroidetes",
+                                                 "Actinobacteria", "Proteobacteria",
+                                                 "Fusobacteria")) 
+                  
+#figure 2, panel B                                                                                "Fusobacteria")) 
+b <- ggplot(Topfive_trtdatar, aes(x=Pipeline, y=mean, fill=Phylum)) + 
+  geom_bar(stat = 'identity', position = position_stack()) +
+  xlab("Rectum") + ylab("Relative Abundance (> 1%)") +
+  scale_x_discrete(labels=c('MG-RAST' = 'MG', 'mothur' = 'M', "QIIME2" = 'Q')) +
+  scale_fill_manual(values=  c("#FF0000", "#006699", "#FF6600", "#FFCC00", "#00A08A")) +
+  theme(legend.position="left")
+b
+
+# alpha div ---------------------------------------------------------------
+
+
+#Alpha Diversity
+erich <- estimate_richness(physeq, measures = c("Observed", "Shannon", "InvSimpson"))
+erich <- add_rownames(erich, "SampleID")
+
+#make data tidy 
+erich2 <- erich %>%
+  gather(Index, Observation, c("Observed", "Shannon", "InvSimpson"), na.rm = TRUE)
+
+#add metadata
+rich = merge(erich2, metadata)
+rich <- rich %>% select(SampleID, Index, Observation, SampleName, Pipeline,
+                        Sample_Area)
+
+rich$Index <- factor(rich$Index, levels = c("Observed", "Shannon", "InvSimpson"))
+#figure 2, panel C
+p <- ggplot(rich, aes(x=Pipeline, y=Observation, fill=Pipeline)) +
+  geom_boxplot(outlier.size = 3)
+c <- p + facet_grid(Index~Sample_Area, scales="free") + scale_fill_manual(values = c("#787878", "#ffb31a", "#5c5c8a")) +
+  scale_x_discrete(labels=c('MG-RAST' = 'MG', 'mothur' = 'M', "QIIME2" = 'Q')) +
+  xlab("") + ylab('Counts') + theme_bw(base_size = 20) + theme(panel.grid.major = element_blank(),
+                                                         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(legend.position = "top")
+
+
+# beta div ----------------------------------------------------------------
+
+
+#beta diversity
+#ellipses indicate sample area
+ord = ordinate(physeq, method="PCoA", distance="(A+B-2*J)/(A+B-J)")
+ordplot=plot_ordination(physeq, ord, color="Pipeline")
+#figure 2, panel D
+d <- ordplot+ geom_point(size = 3,aes(color = sample_data(physeq)$Pipeline)) + scale_color_manual('Legend' ,values = c("#787878", "#ffb31a", "black", "#5c5c8a", "#918151")) +
+  stat_ellipse(alpha = 1, size=2, aes(color= Sample_Area)) +
+  theme(legend.position = 'top')
+d
+
+
+# figure ------------------------------------------------------------------
+
+
+#Final Figure
+theme_set(theme_classic(base_size = 18))
+tiff("Fig2.TIF", width = 4600, height = 4000, res=300)
+ggarrange(a,b,c,d + rremove("x.text"), 
+          labels = c("A", "B", "C", "D"),
+          nrow = 2, ncol = 2)
+dev.off()
+
---
+++ pipeline_comparison/fig3.R
@@ -0,0 +1,199 @@
+#packages
+library(gridExtra)
+library(ggforce)
+library(ggplot2)
+library(ggpubr)
+library(ggthemes)
+library(phyloseq)
+library(plyr)
+library(tidyverse)
+
+tax_group <- read.csv("tax_group_insilico.csv")
+tax_group <- tax_group %>% group_by(Kingdom, Phylum, Class, Order, Family, Genus) %>% summarise_all(funs(sum))
+tax_group <- tax_group %>%  ungroup()
+
+regroup_physeq_object <-function(table) {
+  tax <- table %>% select(Kingdom, Phylum, Class, Order, Family, Genus)
+  tax <- as.matrix(tax)
+  otu <- table %>% select(contains("HMP"))
+  OTU=otu_table(otu, taxa_are_rows=TRUE)
+  TAX=tax_table(tax)
+  taxa_names(TAX)=row.names(OTU)
+  physeq_all=phyloseq(OTU,TAX)
+  return(physeq_all)
+}
+
+physeq_all <- regroup_physeq_object(tax_group)
+
+#import metadata and combine
+metadata=read.csv("insilico/mock3/mock3_meta_merge.csv",header=TRUE)
+sampdat=sample_data(metadata)
+sample_names(sampdat)=metadata$SampleID
+physeq=merge_phyloseq(physeq_all, sampdat)
+physeq=rarefy_even_depth(physeq)
+
+#rarefied level
+otu <- as.data.frame(otu_table(physeq))
+seq_num <- sum(otu[,3])
+
+df <- read.csv("rel_abundance_insilico_asis.csv", header=T)
+new_df <- df[,c("Sample", "Abundance", "Pipeline", "Kingdom", "Phylum", "Class", 
+                "Order", "Family")]
+new_df <- new_df %>% group_by(Sample, Pipeline, Kingdom, Phylum, Class, Order, Family) %>% summarise_all(funs(sum))
+
+#excel sheet from mockrobiota website
+#https://github.com/caporaso-lab/mockrobiota
+taxa_answers <- read.csv('insilico/taxa_answers_nogenus.csv', header=T)
+taxa_answers <- taxa_answers %>%  mutate(Expected = Expected * seq_num)
+
+mg <- new_df %>% filter(Pipeline == 'MG-RAST')
+mot <- new_df %>% filter(Pipeline == 'mothur')
+qim <- new_df %>% filter(Pipeline == 'QIIME2')
+
+compare_mg <- merge(mg, taxa_answers, all= T)
+compare_mot <- merge(mot, taxa_answers, all= T)
+compare_qim <- merge(qim, taxa_answers, all= T)
+compare_mg[is.na(compare_mg)] <- 0
+compare_mot[is.na(compare_mot)] <- 0
+compare_qim[is.na(compare_qim)] <- 0
+
+compare_mg_even1 <- compare_mg %>% filter(Sample == 'HMPMockV1.1.Even1')
+compare_mg_even2 <- compare_mg %>% filter(Sample == 'HMPMockV1.1.Even2')
+compare_mg_stag1 <- compare_mg %>% filter(Sample == 'HMPMockV1.2.Staggered1')
+compare_mg_stag2 <- compare_mg %>% filter(Sample == 'HMPMockV1.2.Staggered2')
+
+compare_mot_even1 <- compare_mot %>% filter(Sample == 'HMPMockV1.1.Even1')
+compare_mot_even2 <- compare_mot %>% filter(Sample == 'HMPMockV1.1.Even2')
+compare_mot_stag1 <- compare_mot %>% filter(Sample == 'HMPMockV1.2.Staggered1')
+compare_mot_stag2 <- compare_mot %>% filter(Sample == 'HMPMockV1.2.Staggered2')
+
+compare_qim_even1 <- compare_qim %>% filter(Sample == 'HMPMockV1.1.Even1')
+compare_qim_even2 <- compare_qim %>% filter(Sample == 'HMPMockV1.1.Even2')
+compare_qim_stag1 <- compare_qim %>% filter(Sample == 'HMPMockV1.2.Staggered1')
+compare_qim_stag2 <- compare_qim %>% filter(Sample == 'HMPMockV1.2.Staggered2')
+
+find_taxa_errors <- function(dataframe) {
+  correct_taxa <- c()
+  false_positive <- c()
+  false_negative <- c()
+  for(i in 1:nrow(dataframe)) {
+    if (dataframe$Abundance[[i]] > 0 & dataframe$Expected[[i]] > 0) {
+      correct_taxa<- append(correct_taxa, 1)
+    } else if (dataframe$Abundance[[i]] == 0 & dataframe$Expected[[i]] > 0) {
+      false_negative <-append(false_negative, 1)
+    } else if (dataframe$Abundance[[i]] > 0 & dataframe$Expected[[i]]== 0) {
+      false_positive <- append(false_positive, 1)
+    }
+  }
+  a <- c(sum(correct_taxa), sum(false_negative), sum(false_positive))
+  return(a)
+}
+
+mg_even1 <- find_taxa_errors(compare_mg_even1)
+mg_even2 <- find_taxa_errors(compare_mg_even2)
+mg_stag1 <- find_taxa_errors(compare_mg_stag1)
+mg_stag2 <- find_taxa_errors(compare_mg_stag2)
+
+mot_even1 <- find_taxa_errors(compare_mot_even1)
+mot_even2 <- find_taxa_errors(compare_mot_even2)
+mot_stag1 <- find_taxa_errors(compare_mot_stag1)
+mot_stag2 <- find_taxa_errors(compare_mot_stag2)
+
+qim_even1 <- find_taxa_errors(compare_qim_even1)
+qim_even2 <- find_taxa_errors(compare_qim_even2)
+qim_stag1 <- find_taxa_errors(compare_qim_stag1)
+qim_stag2 <- find_taxa_errors(compare_qim_stag2)
+
+counts <- c(mg_even1, mg_even2, mg_stag1, mg_stag2, mot_even1, mot_even2, mot_stag1,
+                   mot_stag2, qim_even1, qim_even2, qim_stag1, qim_stag2)
+names <- c(rep("mg_even1", 3), rep("mg_even2",3), rep("mg_stag1",3), rep("mg_stag2",3), 
+           rep("mot_even1", 3), rep("mot_even2",3), rep("mot_stag1",3), rep("mot_stag2",3), 
+           rep("qim_even1", 3), rep("qim_even2",3), rep("qim_stag1",3), rep("qim_stag2",3))
+taxa_Data <- data.frame(cbind(names, counts))
+taxa_Data$Pipeline <- 'QIIME2'
+taxa_Data$Pipeline[grep('mot', taxa_Data$names)] = 'mothur'
+taxa_Data$Pipeline[grep('mg', taxa_Data$names)] = 'MG-RAST'
+taxa_Data$Community <- 'Even'
+taxa_Data$Community[grep('stag', taxa_Data$names)] = 'Staggered'
+taxa_Data$Sample_number <- factor(1:36)
+taxa_Data$Id <- rep(c("Correct Taxa", "False Negatives", "False Positives"), 12)
+taxa_Data$counts <- as.numeric(as.character(taxa_Data$counts))
+
+a <- ggplot(data = taxa_Data, aes(x = Sample_number, y = counts, fill = Pipeline)) + 
+  geom_bar(stat = 'identity') + facet_wrap(~Id, scales = 'free') + xlab("") +
+  scale_fill_manual(values=c("#787878", "#ffb31a", "#5c5c8a")) + ylab("Counts")
+a <- a  + theme(axis.text.x = element_blank()) +
+  theme(legend.position="top")
+a
+
+changing_false_negatives <- function(dataframe) {
+  for(i in 1:nrow(dataframe)) {
+    if(dataframe$Abundance[[i]] > 0 & dataframe$Expected[[i]] == 0) {
+      dataframe$Abundance[[i]] <- -dataframe$Abundance[[i]]
+    }
+  }
+  return(dataframe)
+}
+
+compare_mg <- changing_false_negatives(compare_mg)
+
+ggplotRegression <- function(fit){
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point(color = 'grey', size = 4) + 
+    geom_abline(slope =1, intercept = 0, linetype = 3) +
+    geom_abline(slope =0, intercept = 0, color='black') +
+    geom_vline(xintercept = 0, color='black') +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste('MG-RAST')) +
+    annotate('text', label = "Adjusted R-squared = 0.238",
+                                              x = 1500, y = 200)
+}
+
+b <- ggplotRegression(lm(Expected ~ Abundance, data = compare_mg))
+b
+
+compare_mot <- changing_false_negatives(compare_mot)
+
+ggplotRegression <- function(fit){
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point(color = "#ffb31a", size = 4) + 
+    geom_abline(slope =1, intercept = 0, linetype = 3) +
+    geom_abline(slope =0, intercept = 0, color='black') +
+    geom_vline(xintercept = 0, color='black') +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste('mothur')) +
+    annotate('text', label = "Adjusted R-squared = 0.083",
+             x = -1000, y = 800)
+    
+}
+
+c <- ggplotRegression(lm(Expected ~ Abundance, data = compare_mot))
+c
+
+
+compare_qim <- changing_false_negatives(compare_qim)
+
+ggplotRegression <- function(fit){
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point(color = "#5c5c8a", size = 4) + 
+    geom_abline(slope =1, intercept = 0, linetype=3) +
+    geom_abline(slope =0, intercept = 0, color='black') +
+    geom_vline(xintercept = 0, color='black') +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste('QIIME2')) +
+    annotate('text', label = "Adjusted R-squared = 0.267",
+             x = 1200, y = 200)
+}
+
+d <- ggplotRegression(lm(Expected ~ Abundance, data = compare_qim))
+d
+
+theme_set(theme_classic(base_size = 18))
+tiff("Fig3.TIF", width = 4000, height = 3500, res=300)
+ggarrange(a,b,c,d , 
+          labels = c("A", "B", "C", "D"),
+          nrow = 2, ncol = 2)
+dev.off()
